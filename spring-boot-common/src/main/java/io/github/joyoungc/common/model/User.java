7432d2b04addb7e5712c1bb29b825965a16e8001
Jo Young Jeong <balhae79@gmail.com>
2018-04-03 16:38:48 +0900

    Update Spring Boot Version

--- spring-boot-common/src/main/java/io/github/joyoungc/common/model/User.java
+++ spring-boot-common/src/main/java/io/github/joyoungc/common/model/User.java
@@ -1,48 +1,11 @@
 package io.github.joyoungc.common.model;
 
-public class User {
+import lombok.Data;
 
+@Data
+public class User {
 	private String userId;
 	private String userName;
 	private Integer userNumber;
 	private String birthday;
-
-	public String getUserId() {
-		return userId;
-	}
-
-	public void setUserId(String userId) {
-		this.userId = userId;
-	}
-
-	public String getUserName() {
-		return userName;
-	}
-
-	public void setUserName(String userName) {
-		this.userName = userName;
-	}
-
-	public Integer getUserNumber() {
-		return userNumber;
-	}
-
-	public void setUserNumber(Integer userNumber) {
-		this.userNumber = userNumber;
-	}
-
-	public String getBirthday() {
-		return birthday;
-	}
-
-	public void setBirthday(String birthday) {
-		this.birthday = birthday;
-	}
-
-	@Override
-	public String toString() {
-		return "User [userId=" + userId + ", userName=" + userName + ", userNumber=" + userNumber + ", birthday="
-				+ birthday + "]";
-	}
-
 }
--- spring-boot-swagger/README.md
+++ spring-boot-swagger/README.md
@@ -1,7 +1,7 @@
 # Spring Boot with Swagger
 Spring Boot에 Swagger를 적용한 예제 프로젝트 입니다.
 - 개발된 API를 HTML문서로 실시간으로 반영되며, 화면단에서 바로 테스트도 진행할 수 있습니다.  
-- 화면은 Spring Boot 실행 후 http://localhost:8080/swagger-ui.html 에서 확인이 가능합니다.
+- 화면은 Spring Boot 실행 후 http://localhost:9020/swagger-ui.html 에서 확인이 가능합니다.
 - [spring-boot-common](https://github.com/joyoungc/spring-boot-sample-project/tree/master/spring-boot-common) 를 dependency로 추가해야 합니다.
 
 ## 개발환경
